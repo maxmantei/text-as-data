03065d3a60ba7f4e071c3dcf1671cb342b049fb6
Max Mantei <max.mantei@gmail.com>
2018-03-22 15:28:07 +0100

    very minor changes

--- code/Exercise_6_classification.R
+++ code/Exercise_6_classification.R
@@ -1,56 +1,61 @@
 # Ulrich Fritsche
 # based on code by Gregor Wiedemann and Andreas Niekler and Pablo Barbera (LSE)
 # Jan 22, 2018
-# --------------------------------------
-# Housekeeping & Path
-# --------------------------------------
-
-
-# Be aware that all necessary packages are installed
 #
+# slightly changed by Maximillain Mantei @ 2018/03/22
+#
+# --------------------------------------
+# Housekeeping & Path (easier with R project)
+# --------------------------------------
 
-rm(list=ls())
-# check working directory. It should be the destination folder of the extracted 
-# zip file. If necessary, use `setwd("your-tutorial-folder-path")` to change it.
-
-setwd("C:/Users/FritscheU/Dropbox/RCourse_Text_Mining/Code")
-getwd() # check that the folder is correct
-
-# important option for text analysis
+# set options
 options(stringsAsFactors = F)
 
+# load packages
+library(tidyverse)
 library(tm)
+library(LiblineaR)
+library(SparseM)
+library(quanteda)
+library(glmnet)
+library(doParallel) # parallel computing in Windows
+registerDoParallel(3) # I use 3 out of my 4 cores
 
-textdata <- read.csv("data/sotu_paragraphs.csv", sep = ";", encoding = "UTF-8")
-english_stopwords <- readLines("resources/stopwords_en.txt", encoding = "UTF-8")
+# load custom function(s)
+source("code/utils.R")
+# Function for Precision, Recall, Specificity, F value, Accuracy
+source("code/F.measure.R") ## here c-optimisation is included
+
+textdata <- read.csv("code/data/sotu_paragraphs.csv", sep = ";", encoding = "UTF-8")
+english_stopwords <- readLines("code/resources/stopwords_en.txt", encoding = "UTF-8")
 
 # Create corpus object
-names(textdata)[names(textdata) == "id"] <- "doc_id"
-textdata <- textdata[c(2,6,1,3,4,5)]
+names(textdata)[names(textdata) == "p_id"] <- "doc_id"
+textdata <- textdata[c(1,6,2,3,4,5)]
 
 corpus <- Corpus(DataframeSource(textdata))
 
 # Preprocessing chain
-processedCorpus <- tm_map(corpus, removePunctuation, preserve_intra_word_dashes = TRUE)
-processedCorpus <- tm_map(processedCorpus, removeNumbers)
-processedCorpus <- tm_map(processedCorpus, content_transformer(tolower))
-processedCorpus <- tm_map(processedCorpus, stripWhitespace)
+processedCorpus <- corpus %>%
+  tm_map(removePunctuation, preserve_intra_word_dashes = TRUE) %>%
+  tm_map(removeNumbers) %>%
+  tm_map(content_transformer(tolower)) %>%
+  tm_map(stripWhitespace)
 
 # Read previously annotated training data
-trainingData <- read.csv2("data/paragraph_training_data_format.csv", stringsAsFactors = T)
+trainingData <- read.csv2("code/data/paragraph_training_data_format.csv", stringsAsFactors = T)
 # Training data format 
 colnames(trainingData)
 
 # Example paragraph annotated as "Foreign Affairs"
 set.seed(13)
 domestic_example <- sample(trainingData$ID[trainingData$LABEL == "DOMESTIC"], 1)
-as.character(corpus[[domestic_example]])
+as.character(processedCorpus[[domestic_example]])
 
 foreign_example <- sample(trainingData$ID[trainingData$LABEL == "FOREIGN"], 1)
-as.character(corpus[[foreign_example]])
+as.character(processedCorpus[[foreign_example]])
 
 #How is the ratio between domestic and foreign content in the training data?
-
 classCounts <- table(trainingData[, "LABEL"])
 print(classCounts)
 
@@ -66,20 +71,21 @@ minimumFrequency <- 5
 DTM <- DocumentTermMatrix(processedCorpus, control = list(bounds = list(global = c(minimumFrequency, Inf))))
 dim(DTM)
 
+#-------------------------------------------------------------------------------
 # we build a linear SVM classification model with the LiblineaR package.
-require(LiblineaR)
-require(SparseM)
-source("utils.R")
 
-# First, we load the packages. Since Liblinear requires a special Sparse Matrix format, we also load the “SparseM” package and a conversion function which allows to convert slam-matrices (as used in the tm package) into SparseM-matrices.
-# Then, we split the annotated data into a training set (80%) and a test set (20%) using a boolean selector. The expression assigned to selector_idx creates a boolean vector of length 300 containing a FALSE value in every fifths position. This selector is used to select to training set. Its inverted vector (!) is used to select the test set.
-
+# First, we load the packages. Since Liblinear requires a special Sparse Matrix format, 
+# we also load the “SparseM” package and a conversion function which allows to convert slam-matrices (as used in the tm package) into SparseM-matrices.
+# Then, we split the annotated data into a training set (80%) and a test set (20%) using a boolean selector. 
+# The expression assigned to selector_idx creates a boolean vector of length 300 containing a FALSE value in every fifths position. 
+# This selector is used to select to training set. Its inverted vector (!) is used to select the test set.
 annotatedDTM <- DTM[trainingData[, "ID"], ]
 annotatedDTM <- convertSlamToSparseM(annotatedDTM)
 annotatedLabels <- trainingData[, "LABEL"]
 
 # split into training and test set
-selector_idx <- rep(c(rep(TRUE, 4), FALSE), length.out = numberOfDocuments) # rep() replicates, length.out definiert Länge, generiert eine Dummy-Variable mit 111101111011110...
+# rep() replicates, length.out definiert Länge, generiert eine Dummy-Variable mit 111101111011110...
+selector_idx <- rep(c(rep(TRUE, 4), FALSE), length.out = numberOfDocuments) 
 trainingDTM <- annotatedDTM[selector_idx, ]
 trainingLabels <- annotatedLabels[selector_idx]
 testDTM <- annotatedDTM[!selector_idx, ]
@@ -89,8 +95,8 @@ testLabels <- annotatedLabels[!selector_idx]
 model <- LiblineaR(trainingDTM, trainingLabels, type=2)
 summary(model)
 
-# The model created by the LiblineaR function can now be utilized to predict the labels of the test set. Then we compare the result of the automatic classification to our known labels to determine the accuracy of the process.
-
+# The model created by the LiblineaR function can now be utilized to predict the labels of the test set. 
+# Then we compare the result of the automatic classification to our known labels to determine the accuracy of the process.
 classification <- predict(model, testDTM) 
 predictedLabels <- classification$predictions
 contingencyTable <- table(predictedLabels, testLabels)
@@ -98,9 +104,6 @@ print(contingencyTable) # contingency table
 
 accuracy <- sum(diag(contingencyTable)) / length(testLabels)
 print(accuracy) # Accuracy = fraction of correctly classified elements
-
-# Function for Precision, Recall, Specificity, F value, Accuracy
-source("F.measure.R") ## here c-optimisation is included
 
 # Create pseudo classification
 pseudoLabelsDOM <- factor(rep("DOMESTIC", length(testLabels)), levels(testLabels))
@@ -114,11 +117,13 @@ F.measure(predictedLabels, testLabels, positiveClassName = "FOREIGN")
 F.measure(pseudoLabelsDOM, testLabels, positiveClassName = "DOMESTIC")
 F.measure(pseudoLabelsDOM, testLabels, positiveClassName = "FOREIGN")
 
-# If classes in training/test sets are imbalanced, accuracy might be a misleading measurement. Other measure should be considered additionally.
-# To utilize accuracy and F-measure in a meaningful way, the less frequent class should be defined as POSITIVE class (FOREIGN in our case).
+# If classes in training/test sets are imbalanced, accuracy might be a misleading measurement. 
+# Other measure should be considered additionally. To utilize accuracy and F-measure in a meaningful way, 
+# the less frequent class should be defined as POSITIVE class (FOREIGN in our case).
 
+#-------------------------------------------------------------------------------
+# Kfold cv
 # introduce another function
-
 get_k_fold_logical_indexes <- function(j, k, n) {
   if (j > k) stop("Cannot select fold larger than nFolds")
   fold_lidx <- rep(FALSE, k)
@@ -142,7 +147,7 @@ for (j in 1:k) {
   foldLabels <- annotatedLabels[!currentFold]
   
   # create model
-  model <- LiblineaR(foldDTM, foldLabels)
+  model <- LiblineaR(foldDTM, foldLabels, type = 2)
   
   # select test data split
   testSet <- annotatedDTM[currentFold, ]
@@ -159,11 +164,9 @@ for (j in 1:k) {
 }
 # Final evaluation values of k runs:
 print(evalMeasures)
-
 print(colMeans(evalMeasures))
 
 # C parameter
-
 cParameterValues <- c(0.003, 0.01, 0.03, 0.1, 0.3, 1, 3 , 10, 30, 100)
 fValues <- NULL
 
@@ -174,12 +177,10 @@ for (cParameter in cParameterValues) {
 }
 
 # plot result
-
 plot(fValues, type="o", col="green", xaxt="n")
 axis(1,at=1:length(cParameterValues), labels = cParameterValues)
 
 # best value
-
 bestC <- cParameterValues[which.max(fValues)]
 print(paste0("Best C value: ", bestC, ", F1 = ", max(fValues)))
 
@@ -187,14 +188,8 @@ print(paste0("Best C value: ", bestC, ", F1 = ", max(fValues)))
 # Another (smart) approach: Pablo Barbera (LSE)
 ###########################################################################################
 
-# setwd("C:/Users/FritscheU/Dropbox/RCourse_Text_Mining/Code/data/")
-
-library(quanteda)
-#tweets <- read.csv("./EP-elections-tweets.csv", stringsAsFactors=F)
-#save(tweets, file = "data/tweets.RData")
-
 # Due to some compatibility issues, we saved the data in original R format
-load("data/tweets.RData")
+load("code/data/tweets.RData")
 
 # dummy for polite
 tweets$impolite <- ifelse(tweets$polite=="polite", 0, 1)
@@ -216,12 +211,6 @@ set.seed(123)
 training <- sample(1:nrow(tweets), floor(.80 * nrow(tweets)))
 test <- (1:nrow(tweets))[1:nrow(tweets) %in% training == FALSE]
 
-library(glmnet)
-library(doParallel) # parallel computing in Windows
-registerDoParallel(3) # I use 3 out of my 4 cores
-# 
-#require(doMC) # linux
-#registerDoMC(cores=3)
 ridge <- cv.glmnet(twdfm[training,], tweets$impolite[training], 
                    family="binomial", alpha=0, nfolds=5, parallel=T,
                    type.measure="deviance")
